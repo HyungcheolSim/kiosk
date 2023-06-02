670b51234a0a6a836ef13e95cb65d62a714af19f
HyungCheolSim <sim34122@gmail.com>
2023-06-02 12:16:15 +0900

    annotate main exit code

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
         KioskApp.init();
         while (true) {
             KioskApp.start();
-            Scanner scan = new Scanner(System.in);
+/*          Scanner scan = new Scanner(System.in);
             System.out.println("종료하고 싶으시면 exit 을 입력해주세요");
             System.out.println("다시 시작하고 싶으시면 exit 제외한 문자를 입력해주세요");
             String exit = scan.next().toLowerCase();
             if (exit.equals("exit")) {
                 System.out.println("키오스크 시스템이 종료됩니다. Goodbye");
                 break;
-            }
+            }*/
         }
     }
 }
